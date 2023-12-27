c92da4526b62764379b136bf583264f4aaacc94a
Jatin Parmar <jatinparmar980@gmail.com>
2023-12-27 23:04:00 +0530

    Time: 123 ms (44.11%), Space: 95.6 MB (83.31%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -3,8 +3,6 @@ public:
 	int minCost(string s, vector<int>& time) {
 		int n=s.size();
 		int last=0,ans=0;
-
-		// ans here represent the last value and add current-minimum value to it
 		for(int curr=1;curr<n;++curr){
 			if(s[curr]==s[last]){
 				if(time[last]<time[curr]){
