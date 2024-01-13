d67118ee3ee0b7c6784cc3958c3ef1fbcb7d46ec
Jatin Parmar <jatinparmar980@gmail.com>
2024-01-13 23:52:59 +0530

    Time: 52 ms (65.38%), Space: 17.1 MB (36.03%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minSteps(std::string s, std::string t) {
-        std::vector<int> count_s(26, 0);
-        std::vector<int> count_t(26, 0);
+        vector<int> count_s(26, 0);
+        vector<int> count_t(26, 0);
 
         for (char ch : s) {
             count_s[ch - 'a']++;
@@ -14,7 +14,7 @@ public:
 
         int steps = 0;
         for (int i = 0; i < 26; i++) {
-            steps += std::abs(count_s[i] - count_t[i]);
+            steps += abs(count_s[i] - count_t[i]);
         }
 
         return steps / 2;  
