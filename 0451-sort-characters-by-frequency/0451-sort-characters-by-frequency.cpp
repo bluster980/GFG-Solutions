271b2563abae65583c41d5f7207ee1c21316fbbf
Jatin Parmar <jatinparmar980@gmail.com>
2024-02-07 23:55:31 +0530

    Time: 16 ms (19.18%), Space: 10.8 MB (28.61%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        map<char,int> mp,mp2;
+        for(int i=0; i<s.size(); i++){
+            mp[s[i]]++;
+        }
+        set<pair<int,char>> st;
+        for(auto xx:mp){
+            st.insert({xx.second,xx.first});
+        }
+        string ans;
+        for(auto yy:st){
+            for(int i=0; i<yy.first; i++){
+                ans.push_back(yy.second);
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
